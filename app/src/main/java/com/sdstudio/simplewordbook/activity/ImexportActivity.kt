43a1b6c7102ed892f66dc9912966af35ab1ad768
DoHoon <samdo0812@gmail.com>
2021-02-08 18:55:18 +0900

    setting fragment

---
+++ app/src/main/java/com/sdstudio/simplewordbook/activity/ImexportActivity.kt
@@ -0,0 +1,11 @@
+package com.sdstudio.simplewordbook.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImexportActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imexport)
+    }
+}
